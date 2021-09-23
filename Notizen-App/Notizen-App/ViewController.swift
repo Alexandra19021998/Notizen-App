524ee8db54027ae45e5558d2ef3b62bcce1847b9
Projekt angelegt <Alexandra.Soglassova@edu.bib.de>
2021-09-23 13:23:36 +0200

    Projekt angelegt

---
+++ Notizen-App/Notizen-App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notizen-App
+//
+//  Created by Alexandra Soglassova / BBM3H19M on 23.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
